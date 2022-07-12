96a210657baa838c4110b2a6b79fe2762b3ee08e
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-07-12 09:55:30 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,9 @@
 function updateOutput() {
                 
     $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-    
+    //updating output -- inline html 
     document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
-    
+    //for running javascript code in iframe of output section
     
     
 }
@@ -15,7 +15,7 @@ $(".toggleButton").hover(function() {
 }, function() {
     
     $(this).removeClass("highlightedButton");
-    
+    //hover function (hover documentation)
 });
 
 $(".toggleButton").click(function() {
@@ -29,9 +29,9 @@ $(".toggleButton").click(function() {
     $("#" + panelId).toggleClass("hidden");
     
     var numberOfActivePanels = 4 - $('.hidden').length;
-    
+    //for dividing the panels into equal length in our code
     $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
-    
+   //toggle class documentation 
 })
 
 $(".panel").height($(window).height() - $("#header").height() - 15);
@@ -41,7 +41,7 @@ $(".panel").width(($(window).width() / 2) - 10);
 updateOutput();
 
 $("textarea").on('change keyup paste', function() {
-
+    //this function will trigger the output section as soon as we write the code
     updateOutput();
     
     
